8891f90cd1d1c761d83edc82b9b07823dec66fd7
Wilson Wu <wuyecheng738@gmail.com>
2022-11-21 16:33:12 -0500

    finished my_select

--- enumerables.rb
+++ enumerables.rb
@@ -1,5 +1,4 @@
 class Array
-
     #self = [1, 2, 3]
     def my_each(&prc)
         i = 0
@@ -10,11 +9,27 @@ class Array
         self
     end
 
+    def my_select(&prc)
+        new_arr = []
+        self.my_each do |ele|
+            new_arr << ele if prc.call(ele)  
+        end
+        new_arr 
+    end
+
+
+    
 
 end
 
-return_value = [1, 2, 3].my_each do |num|
- puts num
-end.my_each do |num|
- puts num
-end
+
+# return_value = [1, 2, 3].my_each do |num|
+#  puts num
+# end.my_each do |num|
+#  puts num
+# end
+
+
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
